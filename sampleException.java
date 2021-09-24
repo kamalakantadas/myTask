644bd20de0cc2680a53bd12b532803db55794922
kk27 <kamalakantadas27@gmail.com>
2021-09-24 23:58:09 +0530

    NumberFormatException

--- sampleException.java
+++ sampleException.java
@@ -1,11 +1,14 @@
 class SampleException{
     public static void main(String[] args){
         try{
-            int x = 5/0;
-
+            //int x = 5/0; 
+            int x = Integer.parseInt("hello");
         }
         catch(ArithmeticException e){
             System.out.println("Number cant be divided to zero");
+        }
+        catch(NumberFormatException e){
+            System.out.println("String cant be assigned to int");
         }
     }
 }
