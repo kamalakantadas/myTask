54d97f83fa3dafc8f14bd1464dc227a219f4c464
kk27 <kamalakantadas27@gmail.com>
2021-09-25 00:52:39 +0530

    NullPointerException

---
+++ ExceptionNullPointer.java
@@ -0,0 +1,15 @@
+class ExceptionNullPointer{
+	public static void main(String[] args){
+		String[] arElement = new String[5];
+		System.out.println(arElement[0]);
+try{
+	System.out.println(arElement[0].length());
+}catch(NullPointerException e){
+	System.out.println("NullPointer Exception caught"+e);
+}
+catch(Exception e){
+		System.out.println("Exception at last before Finally clause");
+	}
+		
+	}
+}
